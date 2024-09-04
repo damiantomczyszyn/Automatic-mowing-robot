24c3cdaa10415c8298809b088d38f4f90dc3b193
Damian Tomczyszyn <damitom@o2.pl>
2024-09-04 14:31:00 +0200

    Initial code

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,4 +1,33 @@
+#define LEFT_ENGINE 9
+#define RIGHT_ENGINE 10
+#define DRIVE_FAST 200
+#define DRIVE_SLOW 100
+#define LEFT_ENGINE_DIRECTION 11
+#define RIGHT_ENGINE_DIRECTION 12
+//Which is rotated against the direction?
 
+void stop(){
+}
+
+void goForward(){
+
+}
+
+void goBackward(){
+
+}
+void turnRight(){
+
+}
+void turnLeft(){
+
+}
+void turnOnMowingEngine(){
+
+}
+void turnOffMowingEngine(){
+  
+}
 
 
 void setup() {
